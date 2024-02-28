2cc89ab7db92193b9c3192875e88246334b3dc4e
ntsakozinhle <ntsakosambo1@gmail.com>
2024-02-27 22:00:08 -0800

    Repetition Token #0

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+regex = /\bhbt*n\b/
+
+matches = ARGV[0].scan(regex)
+
+if matches.empty?
+  puts ""
+else
+  puts "#{matches.join("\n")}"
+end
