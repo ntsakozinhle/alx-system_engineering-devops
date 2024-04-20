c1ee1c614398b01a3b11abe1f42b69e9a87bd049
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-20 04:44:24 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,12 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+# Notify the execution of the command so it's visible
+
+exec { 'flask_version':
+  command   => '/usr/bin/flask --version',
+  path      => ['/usr/bin'],
+  logoutput => true,
+  require   => Package['flask'],
+}
